d03f4dca6d458e0edfed57c2b950067e491da566
patch <patch@yahoo.fr>
2023-07-25 22:41:26 +0200

    redirect page register

---
+++ App1/forms.py
@@ -0,0 +1,15 @@
+# forms.py
+# forms.py
+
+from django import forms
+from .models import Client
+
+class Client(forms.ModelForm):
+    class Meta:
+        model = Client
+        fields = ['name', 'age']  # Include other fields from the model if needed
+        widgets = {
+            'useragent': forms.HiddenInput(),  # Render useragent field as hidden input
+            'ip': forms.HiddenInput(),         # Render ip field as hidden input
+        }
+
